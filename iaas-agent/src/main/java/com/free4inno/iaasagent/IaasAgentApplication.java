7fe0331ec0a718a82efe2d691686a422ef22983f
zhaoht <zhaoht@bupt.edu.cn>
2022-08-20 20:46:32 +0800

    add iaas-core & iaas-agent

---
+++ iaas-agent/src/main/java/com/free4inno/iaasagent/IaasAgentApplication.java
@@ -0,0 +1,13 @@
+package com.free4inno.iaasagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaasAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IaasAgentApplication.class, args);
+    }
+
+}
---
+++ iaas-core/src/main/java/com/free4inno/iaascore/IaasCoreApplication.java
@@ -0,0 +1,13 @@
+package com.free4inno.iaascore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaasCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IaasCoreApplication.class, args);
+    }
+
+}
