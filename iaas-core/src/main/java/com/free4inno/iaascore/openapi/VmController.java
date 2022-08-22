5123f8944ccfcda765df3fab84672b8d3a16da5e
zhaoht <zhaoht@bupt.edu.cn>
2022-08-23 01:41:53 +0800

    add a test api

---
+++ iaas-core/src/main/java/com/free4inno/iaascore/openapi/VmController.java
@@ -0,0 +1,27 @@
+package com.free4inno.iaascore.openapi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Controller
+@RequestMapping(value = "/vm")
+public class VmController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/test", produces = {"application/json", "application/xml"})
+    @ResponseBody
+    public String test(HttpServletRequest request) {
+        log.info("test ==== start ====");
+        log.info("URI " + request.getRemoteAddr() + " " + request.getRemoteHost() + " " + request.getRemotePort());
+        log.info("test ==== end ====");
+        return "hello";
+    }
+
+
+
+}
