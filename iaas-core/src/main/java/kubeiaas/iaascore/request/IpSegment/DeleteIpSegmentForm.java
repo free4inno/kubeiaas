7212464f1e6056ddc8d7ee8896e0c7d502143e68
llzyh <1226516340@qq.com>
2023-03-15 10:06:50 +0800

    <EDIT> edit deleteIpSegment

---
+++ iaas-core/src/main/java/kubeiaas/iaascore/request/IpSegment/DeleteIpSegmentForm.java
@@ -0,0 +1,13 @@
+package kubeiaas.iaascore.request.IpSegment;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.constraints.NotNull;
+
+@Slf4j
+@Data
+public class DeleteIpSegmentForm {
+    @NotNull
+    private Integer ipSegmentId;
+}
